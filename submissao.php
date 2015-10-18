13d73315b1d7aee6de28cddb2a3aa39a97d5863c
Valdir Jr <jr140797@gmail.com>
2015-10-18 20:19:36 -0200

    Algoritmo de Correção Organizado + Design lindu

--- submissao.php
+++ submissao.php
@@ -8,44 +8,85 @@
 
 <body>
 <div class="container text-center">
-    <h2><legend>Resultado</legend></h2></div>
+    <h2>
+        <legend>Resultado</legend>
+    </h2>
+</div>
 
 
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
 require("bd.php");
-$contador=0;
-$acertos=0;
-?>
+$contador = 0;
+$acertos = 0;
+?>A
 
-    <?php
-    foreach($questoes as $atual){
+<?php
+foreach ($questoes as $atual) {
 
-        $contador++;
+    $contador++;
 
-        $certa= $atual["certa"];
-        $marcada=$_POST[$contador];
-        if ($certa==$marcada){
-            $acertos++;
+    $certa = $atual["certa"];
+    $marcada = $_POST[$contador];
+    if ($certa == $marcada) {
+        $acertos++;
 
-        }
+    }
 
-        ?>
-        <div class="container fundinho-cinza questao">
-            <div class="numero-questao">Questao <?php echo $contador;?></div> <?php echo ($atual["enunciado"])."<br>"; ?>
-            <div class="alternativa <?php if ('a'==$certa){echo " certa";} if (('a'==$marcada)&&('a'!=$certa)){ echo " errada";}?>"> <input <?php if ($marcada=='a'){echo "checked ";}?>type="radio" required name="<?php echo($contador);?>" value="a"> <?php echo($atual["a"]); ?></div>
-            <div class="alternativa<?php if ('b'==$certa){echo " certa";} if (('b'==$marcada)&&('b'!=$certa)){ echo " errada";}?>"> <input <?php if ($marcada=='b'){echo "checked ";}?>type="radio" name="<?php echo($contador);?>" value="b"> <?php echo($atual["b"]); ?></div>
-            <div class="alternativa<?php if ('c'==$certa){echo " certa";} if (('c'==$marcada)&&('c'!=$certa)){ echo " errada";}?>">   <input <?php if ($marcada=='c'){echo "checked ";}?>type="radio" name="<?php echo($contador);?>" value="c"> <?php echo($atual["c"]); ?></div>
-            <div class="alternativa<?php if ('d'==$certa){echo " certa";} if (('d'==$marcada)&&('d'!=$certa)){ echo " errada";}?>">   <input <?php if ($marcada=='d'){echo "checked ";}?>type="radio" name="<?php echo($contador);?>" value="d"> <?php echo($atual["d"]); ?></div>
+    ?>
+    <div class="container fundinho-cinza questao">
+        <div class="numero-questao">Questao <?php echo $contador; ?></div> <?php echo ($atual["enunciado"]) . "<br>"; ?>
+        <div class="alternativa ">
+            <input <?php if ($marcada == 'a') {echo "checked ";} ?>type="radio" required name="<?php echo($contador); ?>" value="a">
+                <?php echo($atual["a"]);
+                if (('a' == $marcada) && ('a' != $certa)) {
+                    echo ('   <span style="color: red" class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+                }
+                if ('a'==$certa){
+                    echo ('   <span style="color: green" class="glyphicon glyphicon-ok" aria-hidden="true"></span>');
+                 }
+            ?></div>
+        <div class="alternativa ">
+            <input <?php if ($marcada == 'b') {echo "checked ";} ?>type="radio" required name="<?php echo($contador); ?>" value="b">
+            <?php echo($atual["b"]);
+            if (('b' == $marcada) && ('b' != $certa)) {
+                echo ('   <span style="color: red" class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+            }
+            if ('b'==$certa){
+                echo ('   <span style="color: green" class="glyphicon glyphicon-ok" aria-hidden="true"></span>');
+            }
+            ?></div>
+        <div class="alternativa ">
+            <input <?php if ($marcada == 'c') {echo "checked ";} ?>type="radio" required name="<?php echo($contador); ?>" value="c">
+            <?php echo($atual["c"]);
+            if (('c' == $marcada) && ('c' != $certa)) {
+                echo ('   <span style="color: red" class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+            }
+            if ('c'==$certa){
+                echo ('   <span style="color: green" class="glyphicon glyphicon-ok" aria-hidden="true"></span>');
+            }
+            ?></div>
+        <div class="alternativa ">
+            <input <?php if ($marcada == 'd') {echo "checked ";} ?>type="radio" required name="<?php echo($contador); ?>" value="d">
+            <?php echo($atual["d"]);
+            if (('d' == $marcada) && ('d' != $certa)) {
+                echo ('   <span style="color: red" class="glyphicon glyphicon-remove" aria-hidden="true"></span>');
+            }
+            if ('d'==$certa){
+                echo ('   <span style="color: green" class="glyphicon glyphicon-ok" aria-hidden="true"></span>');
+            }
+            ?></div>
         </div>
 
-    <?php }
-    ?>
+
+<?php }
+?>
 <div class="container">
     <div class="col-lg-6">
-          Acertos : <?php echo ($acertos."/".count($questoes));?>
-</div></div>
+        Acertos : <?php echo($acertos . "/" . count($questoes)); ?>
+    </div>
+</div>
 
 
 </body>
